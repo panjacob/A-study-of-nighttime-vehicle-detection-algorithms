a1a911e4f17cf5e1bfbc42886d39b92da4e6548f
panjacob <kwiatkowskijakub@protonmail.com>
2023-04-07 19:55:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+print(tf.__version__)
+# tf.test.gpu_device_name()
+
+from tensorflow.python.client import device_lib
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+print(get_available_devices())
