89931d6275f4557c3565671880bfd76cf3ec6368
panjacob <kwiatkowskijakub@protonmail.com>
2023-04-12 20:27:16 +0200

    data_unclassified

--- Recordings/inference.py
+++ Recordings/inference.py
@@ -1,3 +1,5 @@
+import csv
+
 import cv2
 from tqdm import tqdm
 import os
@@ -9,17 +11,9 @@ print(video_list_files)
 
 model_yolo8n = os.path.join('..', 'yolov8', 'n50_50_100.onnx')
 model = YOLO(model_yolo8n)
-# results = model.predict(source=)
-# print(results)
-# for result in results:
-#     boxes = result.boxes  # Boxes object for bbox outputs
-#     masks = result.masks  # Masks object for segmentation masks outputs
-#     probs = result.probs  # Class probabilities for classification outputs
-#     print(boxes)
-#     print(masks)
-#     print(probs)
-
-for video_name in video_list_files:
+blocking_lines = [300, 310, 320]
+for i, video_name in enumerate(video_list_files):
+    data = []
     video_path = os.path.join('videos', video_name)
     cap = cv2.VideoCapture(video_path)
     frames_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -43,22 +37,35 @@ for video_name in video_list_files:
         if success_count < 3000:
             continue
 
-        # results = model.predict(source=frame[:, :, ::-1])
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        frame = cv2.resize(frame, (640, 640))
         result = model.predict(source=frame)
+        found = 0
         for x in result[0].boxes.xyxy:
             y = x.numpy().astype(int)
-            print(y)
-            if 640 < y[0] < 700 and 320 < y[1] < 360 and 690 < y[2] < 740 and  330< y[3] < 370:
-                print('pomijam lampe')
+            # print(y)
+            if y[0] > blocking_lines[i]:
+                # print('pomijam lampe')
                 continue
+            found += 1
             frame = cv2.rectangle(frame, (y[0], y[1]), (y[2], y[3]), (255, 0, 0), 2)
-
+        # frame = cv2.line(frame, (0, blocking_lines[i]), (640, blocking_lines[i]), (255, 0, 0), 1)
         # print(result[0].boxes)
+        is_car = found > 0
+        data.append([success_count, is_car])
+
+
 
         pbar.update(1)
-        cv2.imshow('Frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # cv2.imshow('Frame', frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
+    f = open(f"data_inference/data_{i}.csv", 'w', newline='', encoding='utf-8')
+    writer = csv.writer(f)
+    writer.writerows(data)
+    f.close()
 
     # print(succ, fail)
     cap.release()
